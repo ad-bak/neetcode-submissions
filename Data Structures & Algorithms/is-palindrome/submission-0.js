206f29909ef0e58176b8138fe9dff1bb8dca5066
adnan <113508739+ad-bak@users.noreply.github.com>
2026-04-22 01:04:19 +0200

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.js
@@ -0,0 +1,20 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isPalindrome(s) {
+        let clean = s.replace(/[^a-zA-Z0-9]/g, "").toLowerCase();
+
+        let left = 0;
+        let right = clean.length - 1;
+
+        while (left < right) {
+            if (clean[left] !== clean[right]) return false;
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+}
